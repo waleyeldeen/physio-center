9756d8938bc00bfb90455abd07ec55cf3c6d03f4
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-30 03:59:41 +0200

    Add PriQueue.h and PriNode.h

---
+++ PriQueue.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "PriNode.h"
+
+
+//This class impelements the priority queue as a sorted list (Linked List)
+//The item with highest priority is at the front of the queue
+template <typename T>
+class PriQueue
+{
+    PriNode<T>* head;
+public:
+    PriQueue() : head(nullptr) {}
+
+    ~PriQueue() {
+        T tmp;
+        int p;
+        while (dequeue(tmp, p));
+    }
+
+    //insert the new node in its correct position according to its priority
+    void enqueue(const T& data, int priority) {
+        PriNode<T>* newNode = new PriNode<T>(data, priority);
+
+        if (head == nullptr || priority > head->getPri()) {
+
+            newNode->setNext(head);
+            head = newNode;
+            return;
+        }
+
+        PriNode<T>* current = head;
+        while (current->getNext() && priority <= current->getNext()->getPri()) {
+            current = current->getNext();
+        }
+        newNode->setNext(current->getNext());
+        current->setNext(newNode);
+    }
+
+    bool dequeue(T& topEntry, int& pri) {
+        if (isEmpty())
+            return false;
+
+        topEntry = head->getItem(pri);
+        PriNode<T>* temp = head;
+        head = head->getNext();
+        delete temp;
+        return true;
+    }
+
+    bool peek(T& topEntry, int& pri) {
+        if (isEmpty())
+            return false;
+
+        topEntry = head->getItem();
+        pri = head->getPri();
+        return true;
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+};
