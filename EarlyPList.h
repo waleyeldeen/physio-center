37d62e2fb81d44e4cb6efef893be386c2a0ae86b
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-30 19:13:02 +0200

    create class prototype

---
+++ EarlyPList.h
@@ -0,0 +1,12 @@
+#pragma once
+using namespace std;
+
+#include "PriQueue.h"
+
+
+class EarlyPList : public PriQueue<int> // int is temporary unit patient class is created
+{
+	void reschedule() {
+		
+	}
+};
