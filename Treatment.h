2133c3a6f020c1406bf391f7ddc7382df4273703
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-31 22:36:46 +0200

    add assignedResource

--- Treatment.h
+++ Treatment.h
@@ -1,13 +1,26 @@
 #pragma once
 using namespace std;
 #include <iostream>
+#include "Resource.h"
 
 class Treatment
 {
 private:
 	int duration, time;
+	Resource* assignedRes;
 public:
-	Treatment(int duration = 0, int time = 0) : duration(duration), time(time) {}
+	Treatment(int duration = 0, int time = 0) : duration(duration), time(time), assignedRes(nullptr) {}
+
+	void setAssignedRes(Resource* newResource)
+	{
+		assignedRes = newResource;
+		assignedRes->unavailable();
+	}
+
+	void finishTreatment()
+	{
+		assignedRes->available();
+	}
 
 	virtual void dummyAbstract() = 0;
 };
