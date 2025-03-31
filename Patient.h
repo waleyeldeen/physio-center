861aa0f05755278fcea571ff7deb7a8c0d276c60
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-31 17:26:23 +0200

    implement addTreatment()

--- Patient.h
+++ Patient.h
@@ -9,7 +9,8 @@ class Patient
 private:
 	int id, pt, vt;
 
-	LinkedQueue<Treatment*> reqTreatment;
+	// TODO: Ask Eng if this is allowed or not
+	LinkedQueue<Treatment> reqTreatment;  // make reqTreatment made of Treatment obj instead of pointer
 
 	// True: Normal Patient
 	// False: Recovering Patient
@@ -29,6 +30,18 @@ public:
 	void setId(int newId) { id = newId; }
 	void setPt(int newPt) { pt = newPt; }
 	void setVt(int newVt) { vt = newVt; }
+	
+	// returns true if treatment is added succeffuly
+	// returns false if treatment cannot be added (max no of treatments)
+	bool addTreatment(Treatment newT)
+	{
+		if (reqTreatment.getCount() <= 3)
+		{
+			reqTreatment.enqueue(newT);
+			return true;
+		}
+		else return false;
+	}
 
 	// check if a patient has anymore treatments in the reqTreatment list
 	// used by cancel function in XWaitlist
