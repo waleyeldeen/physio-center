5049bdc7553afffabad4715797566e4bab6bc6e6
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-31 20:46:43 +0200

    implement resource abstract class

--- Resource.h
+++ Resource.h
@@ -1,1 +1,18 @@
 #pragma once
+using namespace std;
+#include <iostream>
+#include "DEFS.h"
+
+
+class Resource
+{
+protected:
+	int id;
+	ResourceType type;
+public:
+	int getId() { return id; }
+	ResourceType getType() { return type; }
+
+	virtual void makeAbstract() = 0;
+};
+
