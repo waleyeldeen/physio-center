e7c862caf50c914735be21006aa766e007886ad0
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-31 21:34:55 +0200

    Add setter and constructor

--- Resource.h
+++ Resource.h
@@ -10,8 +10,14 @@ protected:
 	int id;
 	ResourceType type;
 public:
+	Resource(int id = 0, ResourceType type = NONE) : id(id), type(type) {}
+
+	// getters
 	int getId() { return id; }
 	ResourceType getType() { return type; }
+
+	// setters
+	void setId(int newId) { type = newId; }
 
 	virtual void makeAbstract() = 0;
 };
