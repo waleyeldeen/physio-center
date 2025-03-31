ac5a5dc850168299ddad0ae95cbd8ac477a68876
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-03-31 22:36:27 +0200

    add availability member

--- Resource.h
+++ Resource.h
@@ -9,15 +9,20 @@ class Resource
 protected:
 	int id;
 	ResourceType type;
+	bool isAvailable;
 public:
-	Resource(int id = 0, ResourceType type = NONE) : id(id), type(type) {}
+	Resource(int id = 0, ResourceType type = NONE) : id(id), type(type), isAvailable(true) {}
 
 	// getters
 	int getId() { return id; }
 	ResourceType getType() { return type; }
+	bool isAvailable() { return isAvailable; }
 
 	// setters
 	void setId(int newId) { type = newId; }
+
+	void available() { isAvailable = true; }
+	void unavailable() { isAvailable = false; }
 
 	virtual void makeAbstract() = 0;
 };
