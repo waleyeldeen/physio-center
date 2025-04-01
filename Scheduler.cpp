bfac23e36cf4211fdd373fb674380d76a5e38dba
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-04-01 11:35:30 +0200

    Implement list adders

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -1,1 +1,8 @@
 #include "Scheduler.h"
+void Scheduler::addToIdle(Patient* p) { idle.enqueue(p); }
+void Scheduler::addToEarly(Patient* p) { early.enqueue(p); }
+void Scheduler::addToLate(Patient* p) { late.enqueue(p); }
+
+void Scheduler::addToWaitU(Patient* p) { waitU.enqueue(p); }
+void Scheduler::addToWaitE(Patient* p) { waitE.enqueue(p); }
+void Scheduler::addToWaitX(Patient* p) { waitX.enqueue(p); }
