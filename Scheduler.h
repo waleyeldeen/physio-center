01d37a13772d03fe6ab250acaabe2083129e6928
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-04-01 02:00:28 +0200

    add basic data members

--- Scheduler.h
+++ Scheduler.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+#include "LinkedQueue.h"
+#include "EarlyPList.h"
+#include "PriQueue.h"
+#include "UEWaitlist.h"
+#include "XWaitlist.h"
+#include "ArrayStack.h"
+
 class Scheduler
 {
+private:
+	LinkedQueue<Patient*> idle;
+	EarlyPList early;
+	PriQueue<Patient*> late;
+
+	UEWaitlist waitU;
+	UEWaitlist waitE;
+	XWaitlist waitX;
+
+	PriQueue<Patient*> serving;
+
+	LinkedQueue<Patient*> uDevices;
+	LinkedQueue<Patient*> eDevices;
+	LinkedQueue<Patient*> xDevices;
+
+	ArrayStack<Patient*> finish;
 };
 
