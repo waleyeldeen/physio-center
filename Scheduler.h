2b0f97e9b412bc7d15d0910e8bbcaa6b4deaaac7
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-04-01 11:23:51 +0200

    Add f unction prototypes and more data members

--- Scheduler.h
+++ Scheduler.h
@@ -24,8 +24,28 @@ private:
 
 	LinkedQueue<Patient*> uDevices;
 	LinkedQueue<Patient*> eDevices;
-	LinkedQueue<Patient*> xDevices;
+	LinkedQueue<Patient*> xRooms;
 
 	ArrayStack<Patient*> finish;
+
+	int pResc, pCancel, numPatients;
+
+public:
+	Scheduler();
+
+	/*Called at each timestep to check the idle list for patients who
+	arrived and move them to early or late accordingle*/
+	void checkIdleForArrivedPatients();
+
+	void addToEarly(Patient* p);
+	void addToLate(Patient* p);
+
+	void addToWaitU(Patient* p);
+	void addToWaitE(Patient* p);
+	void addToWaitX(Patient* p);
+
+	/*Opens the input file and assigs the data to different data members*/
+	void loadInputFile(string fileName);
+
 };
 
