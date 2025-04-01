80ad43b3032aff7ffa77d61e14e2c8fb50dbd351
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-04-01 13:28:26 +0200

    input devices and create objects

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -6,11 +6,44 @@ Scheduler::Scheduler() { }
 void Scheduler::loadInputFile(string fileName)
 {
 	fstream file(fileName);
-	string line;
 
+	// input number of devices
 	int numEDevices, numUDevices, numXRooms;
 	file >> numEDevices >> numUDevices >> numXRooms;
-	cout << "E: " << numEDevices << " U: " << numUDevices << " X: " << numXRooms;
+	
+	// input capacities of gym rooms
+	int* xCap = new int[numXRooms];
+	for (int i = 0; i < numXRooms; i++)
+		file >> xCap[i];
+
+	// add device objects
+	// Electro Devices
+	for (int i = 0; i < numEDevices; i++)
+	{
+		// for the sake of the code I will make the id of each resource the i*10
+		eDevices.enqueue(new EDevice((i+1) * 10, EDEVICE));
+	}
+
+	// Ultrasound Devices
+	for (int i = 0; i < numUDevices; i++)
+	{
+		// for the sake of the code I will make the id of each resource the i*10
+		uDevices.enqueue(new UDevice((i + 1) * 100, UDEVICE));
+	}
+
+	// Gym Rooms
+	for (int i = 0; i < numXRooms; i++)
+	{
+		// for the sake of the code I will make the id of each resource the i*10
+		xRooms.enqueue(new XRoom((i + 1) * 1000, XROOM));
+	}
+
+	// input pCancel and pResc
+	file >> this->pCancel >> this->pResc;
+	
+
+	
+	file.close();
 }
 
 void Scheduler::checkIdleForArrivedPatients()
