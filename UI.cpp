9612dc537cfea44673bd5a2a2d17c2d7a26f4fc5
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-04-03 05:43:36 +0200

    Print all information (not complete)

--- UI.cpp
+++ UI.cpp
@@ -25,5 +25,48 @@ void UI::printAllInformation(Scheduler& sObj, int ts)
 		<< sObj.getIdle().getCount() << " patients remaining: " << endl;
 	// print idle
 	sObj.getIdle().print(true);
+
+	cout << "=================  Waiting Lists  =================" << endl
+		<< sObj.getWaitE().getCount() << " Electro Therapy Patients: ";
+	sObj.getWaitE().print();
+	cout << sObj.getWaitU().getCount() << " Ultra Therapy Patients: ";
+	sObj.getWaitU().print();
+	cout << sObj.getWaitX().getCount() << " X Therapy Patients: ";
+	sObj.getWaitX().print();
+
+	cout << endl;
+
+	cout << "=================  Early List  =================" << endl
+		<< sObj.getEarly().getCount() << " patients: ";
+	sObj.getEarly().print();
+
+	cout << endl;
+
+	cout << "=================  Late List  =================" << endl
+		<< sObj.getLate().getCount() << " patients: ";
+	sObj.getEarly().print();
+
+	cout << endl;
+
+	cout << "=================  Avail E-Devices  =================" << endl
+		<< sObj.getEDevices().getCount() << " Electro Devices: ";
+	sObj.getEDevices().print();
+
+	cout << endl;
+
+	cout << "=================  Avail U-Devices  =================" << endl
+		<< sObj.getEDevices().getCount() << " Ultra Devices: ";
+	sObj.getUDevices().print();
+
+	cout << endl;
+
+	cout << "=================  Avail X-Rooms  =================" << endl
+		<< sObj.getEDevices().getCount() << " Rooms: ";
+	sObj.getXRooms().print();
+
+	cout << endl;
+
+	cout << "=================  Serving List  =================" << endl
+		<< sObj.getServing().getCount() << endl;
 }
 
