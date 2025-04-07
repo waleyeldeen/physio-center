bc391ee93e07c61c7152893d0f1e64ed75903316
waleyeldeen <waleyeldeenahmed@gmail.com>
2025-04-07 02:03:57 +0200

    fix printing

--- UI.cpp
+++ UI.cpp
@@ -20,53 +20,53 @@ string UI::inInputFileName()
 
 void UI::printAllInformation(Scheduler& sObj, int ts)
 {
-	cout << "Current Timestep: " << ts << endl
-		<< "=================  ALL List  =================" << endl
-		<< sObj.getIdle().getCount() << " patients remaining: " << endl;
-	// print idle
-	sObj.getIdle().print(true);
+    cout << "Current Timestep: " << ts << endl
+        << "=================  ALL List  =================" << endl
+        << sObj.getIdle().getCount() << " patients remaining: " << endl;
+    // print idle
+    sObj.getIdle().print(true);
 
-	cout << "=================  Waiting Lists  =================" << endl
-		<< sObj.getWaitE().getCount() << " Electro Therapy Patients: ";
-	sObj.getWaitE().print();
-	cout << sObj.getWaitU().getCount() << " Ultra Therapy Patients: ";
-	sObj.getWaitU().print();
-	cout << sObj.getWaitX().getCount() << " X Therapy Patients: ";
-	sObj.getWaitX().print();
+    cout << "=================  Waiting Lists  =================" << endl
+        << sObj.getWaitE().getCount() << " Electro Therapy Patients: ";
+    sObj.getWaitE().print();
+    cout << sObj.getWaitU().getCount() << " Ultra Therapy Patients: ";
+    sObj.getWaitU().print();
+    cout << sObj.getWaitX().getCount() << " X Therapy Patients: ";
+    sObj.getWaitX().print();
 
-	cout << endl;
+    cout << endl;
 
-	cout << "=================  Early List  =================" << endl
-		<< sObj.getEarly().getCount() << " patients: ";
-	sObj.getEarly().print();
+    cout << "=================  Early List  =================" << endl
+        << sObj.getEarly().getCount() << " patients: ";
+    sObj.getEarly().print();
 
-	cout << endl;
+    cout << endl;
 
-	cout << "=================  Late List  =================" << endl
-		<< sObj.getLate().getCount() << " patients: ";
-	sObj.getEarly().print();
+    cout << "=================  Late List  =================" << endl
+        << sObj.getLate().getCount() << " patients: ";
+    sObj.getLate().print();
 
-	cout << endl;
+    cout << endl;
 
-	cout << "=================  Avail E-Devices  =================" << endl
-		<< sObj.getEDevices().getCount() << " Electro Devices: ";
-	sObj.getEDevices().print();
+    cout << "=================  Avail E-Devices  =================" << endl
+        << sObj.getEDevices().getCount() << " Electro Devices: ";
+    sObj.getEDevices().print();
 
-	cout << endl;
+    cout << endl;
 
-	cout << "=================  Avail U-Devices  =================" << endl
-		<< sObj.getEDevices().getCount() << " Ultra Devices: ";
-	sObj.getUDevices().print();
+    cout << "=================  Avail U-Devices  =================" << endl
+        << sObj.getUDevices().getCount() << " Ultra Devices: ";
+    sObj.getUDevices().print();
 
-	cout << endl;
+    cout << endl;
 
-	cout << "=================  Avail X-Rooms  =================" << endl
-		<< sObj.getEDevices().getCount() << " Rooms: ";
-	sObj.getXRooms().print();
+    cout << "=================  Avail X-Rooms  =================" << endl
+        << sObj.getXRooms().getCount() << " Rooms: ";
+    sObj.getXRooms().print();
 
-	cout << endl;
+    cout << endl;
 
-	cout << "=================  Serving List  =================" << endl
-		<< sObj.getServing().getCount() << endl;
+    cout << "=================  Serving List  =================" << endl
+        << sObj.getServing().getCount() << endl;
 }
 
